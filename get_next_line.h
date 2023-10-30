ef2630d585f185f375cfcfd3f051fff4fd01e1b1
Lukas Dullinger <ldulling@c4r4p3.42vienna.com>
2023-10-30 17:17:35 +0100

    problematic test

--- get_next_line.h
+++ get_next_line.h
@@ -7,7 +7,7 @@
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 128
+#  define BUFFER_SIZE 10
 # endif
 
 typedef struct	s_list
--- main.c
+++ main.c
@@ -20,6 +20,7 @@ int	main(void)
 {
 	char	*result;
 	int		fd;
+	int		iterations;
 
 	// fd = open("/nfs/homes/ldulling/francinette/tests/get_next_line/fsoares/empty.txt", O_RDONLY);
 
@@ -37,24 +38,26 @@ int	main(void)
 	// 	result = get_next_line(fd);
 	// 	printf("%s", result);
 	// }
+
 	/* LINE BY LINE */
-	int	iterations = 5;
+	iterations = 2;
 	for (int i = 0; i < iterations; i++)
 	{
 		result = get_next_line(fd);
 		printf("|%d: %s", i + 1, result);
 		free(result);
 	}
-
 	close(fd);
+
+	// With BUFFER_SIZE of 10, if there's still a full line in the buffer, but before getting it the file gets closed, it would still return the full line from the buffer once it's called again.
 	fd = open("test_short.txt", O_RDONLY);
-		for (int i = 0; i < iterations; i++)
+	iterations = 1;
+	for (int i = 0; i < iterations; i++)
 	{
 		result = get_next_line(fd);
 		printf("|%d: %s", i + 1, result);
 		free(result);
 	}
-
 	close(fd);
 	return (0);
 }
